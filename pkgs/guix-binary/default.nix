b511cb34067fc7ed9aba115be9f5e8e284a4d6f0
nodokaha <scpeee@wail.ch>
2023-09-12 10:54:11 +0900

    update guix-binary to 1.4.0

--- pkgs/guix-binary/default.nix
+++ pkgs/guix-binary/default.nix
@@ -3,15 +3,15 @@
 
 stdenv.mkDerivation rec {
   name = "guix-binary-${version}";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchurl {
     url =
       "https://ftp.gnu.org/gnu/guix/guix-binary-${version}.${stdenv.targetPlatform.system}.tar.xz";
     sha256 = {
-      "x86_64-linux" = "sha256-xffvtIlHVF5J0tr0jQ+DLzXSqNGfZdqvYHlCWsMnwFA=";
-      "i686-linux" = "sha256-QHOK7hFh8LmRFK+EnNBNqfDNwQ0ia0lqRiHov9uyG2Q=";
-      "aarch64-linux" = "sha256-RE9lNs9ZWWNhI6xxjbm4UHFQLkhVVbLYAb8pNpMIEM4=";
+      "x86_64-linux" = "";
+      "i686-linux" = "";
+      "aarch64-linux" = "";
     }."${stdenv.targetPlatform.system}";
   };
   sourceRoot = ".";
